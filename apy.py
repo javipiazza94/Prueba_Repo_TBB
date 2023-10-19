8503ab32bc4d2503579a29a509df648525c2db20
javipiazza94 <javipiazza94@gmail.com>
2023-10-20 00:35:23 +0200

    Añadimos el codigo 3  a la rama branch-ticket-3-1

--- apy.py
+++ apy.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template
-<<<<<<< HEAD
-from src import covid_dash, hospitals_tb
-=======
 from src import covid_dash, hospitals_tb
 
 app = Flask(__name__)
@@ -10,4 +7,10 @@ def landing_page():
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1991, debug=True)
->>>>>>> branch-ticket-2
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+@app.route("/map")
+def map():
+    return render_template('map.html')
